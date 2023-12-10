b080cbd558b30af1a5d943742999aa68b3cd3744
Marian Stopyra <58875711+Mniok@users.noreply.github.com>
2023-12-10 20:04:47 +0100

    create basic message form

--- musicCatalogVueFrontend/src/components/MessageForm.vue
+++ musicCatalogVueFrontend/src/components/MessageForm.vue
@@ -1,3 +1,35 @@
 <template>
-    <span>here be message form</span>
+    <div>
+        <v-form class="w-100">
+            <v-textarea
+                dark color="indigo lighten-2" 
+                v-model="chatStore.currentMessageContent"
+                outlined
+                placeholder="Pisz..."
+                class="mr-3"
+            />
+        </v-form>
+        <v-btn
+            icon fab medium
+            :disabled="!canMessageBePosted"
+            color="indigo lighten-1"
+        >
+            <v-icon x-large color="indigo lighten-1">
+                mdi-send-circle
+            </v-icon>
+        </v-btn>
+    </div>
 </template>
+
+<script setup lang="ts">
+import { computed } from 'vue';
+import { useChatStore } from '@/store/chat';
+
+const chatStore = useChatStore();
+
+const canMessageBePosted = computed(() => {
+    return chatStore.currentMessageContent.length;
+    // TODO: block if user is muted or banned (though even seeing this shouldn't be possible if banned)
+    // TODO: block if prohibited words detected (1984)
+})
+</script>
