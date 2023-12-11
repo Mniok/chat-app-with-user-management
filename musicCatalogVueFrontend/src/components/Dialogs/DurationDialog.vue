1fb4d5a68159d36a00afe409b12a70b47eb5ef1c
Marian Stopyra <58875711+Mniok@users.noreply.github.com>
2023-12-11 05:07:47 +0100

    duration dialog, muting works from app, styling

--- musicCatalogVueFrontend/src/components/Dialogs/DurationDialog.vue
+++ musicCatalogVueFrontend/src/components/Dialogs/DurationDialog.vue
@@ -18,6 +18,16 @@
                 </v-card-text>
         
                 <v-divider></v-divider>
+
+                <v-text-field
+                    color="indigo darken-2"
+                    v-model="duration"
+                    type="time"
+                    messages="hh:mm"
+                    class="px-10"
+                ></v-text-field>
+
+                <v-divider></v-divider>
         
                 <v-card-actions>
                     <v-spacer/>
@@ -44,15 +54,18 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 const emit = defineEmits<{
-  (e: 'confirm'): void
+    (e: 'confirm', duration: string): void
 }>()
+
+const duration = ref("00:00");
 
 const props = defineProps({
   text: String
 })
 
 function confirmHandler() {
-    emit('confirm');
+    console.log('in durationdialog duration:', duration.value)
+    emit('confirm', duration.value);
     isDialogOpen.value = false;
 }
 
